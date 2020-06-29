118e7e98f0f490cdb5fb142ad38a897664436e83
jiangdewei <jiangdewei02@maoyan.com>
2020-06-29 21:59:13 +0800

    feat: add 用promise限制并发任务

---
+++ js/promise/limit-promise.js
@@ -0,0 +1,39 @@
+/**
+ * promise限制并发任务
+ */
+
+/**
+ * 
+ * @param {Array} arr 
+ * @param {Number} limit 限制任务数量
+ * @param {Function} fn 
+ */
+function promiseLimit(arr,limit,fn){
+    let index = 0;
+    function init(){
+        index++;
+        if(index<=arr.length){
+            fn(index).then(()=>{
+                init()
+            })
+        }
+    }
+    for(let i=0;i<limit;i++){
+        init()
+    }
+}
+
+/**
+ * example
+ */
+
+function load(index){
+    return new Promise((res,rej)=>{
+        setTimeout(()=>{
+            console.log(index);
+            res(); 
+        },3000)
+    })
+}
+
+promiseLimit([1,2,3,4,5,6,7,8,9],3,load)
