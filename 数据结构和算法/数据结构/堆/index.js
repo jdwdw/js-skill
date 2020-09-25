73c6f17cf94b321a1ce215559f139d0ca3742e1a
jiangdewei <jiangdewei02@maoyan.com>
2020-09-25 18:23:29 +0800

    topK

--- 数据结构和算法/数据结构/堆/index.js
+++ 数据结构和算法/数据结构/堆/index.js
@@ -61,3 +61,66 @@ function MaxHeap(array){
          }
      }
  }
+
+
+/**
+ * TOPK
+ */
+let findKthLargest = function(nums, k) {
+    // 从 nums 中取出前 k 个数，构建一个小顶堆
+    let heap = [], i = 0
+    while(i < k) {
+       heap.push(nums[i++]) 
+    }
+    buildHeap(heap, k)
+    
+    // 从 k 位开始遍历数组
+    for(let i = k; i < nums.length; i++) {
+        if(heap[0] < nums[i]) {
+            // 替换并堆化
+            heap[0] = nums[i]
+            heapify(heap, k, 0)
+        }
+    }
+    
+    // 返回堆顶元素
+    return heap[0]
+};
+
+// 原地建堆，从后往前，自上而下式建小顶堆
+let buildHeap = (arr, k) => {
+    if(k === 1) return
+    // 从最后一个非叶子节点开始，自上而下式堆化
+    for(let i = Math.floor(k/2); i>=0 ; i--) {
+        heapify(arr, k, i)
+    }
+}
+
+// 堆化
+let heapify = (arr, k, i) => {
+    // 自上而下式堆化
+    while(true) {
+        let minIndex = i
+        let left = 2*i+1
+        let right = 2*i+2
+        if(left <= k && arr[left] < arr[minIndex]) {
+            minIndex = left
+        }
+        if(right <= k && arr[right] < arr[minIndex]) {
+            minIndex = right
+        }
+        if(minIndex !== i) {
+            swap(arr, i, minIndex)
+            i = minIndex
+        } else {
+            break
+        }
+    }
+}
+
+// 交换
+let swap = (arr, i , j) => {
+    let temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+}
