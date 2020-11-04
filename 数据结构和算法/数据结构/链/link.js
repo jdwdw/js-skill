14d725e9a157276201cf6243d33ebb3d438e4a0d
jiangdewei <jiangdewei02@maoyan.com>
2020-11-04 18:26:27 +0800

    链相关算法

--- 数据结构和算法/数据结构/链/link.js
+++ 数据结构和算法/数据结构/链/link.js
@@ -70,7 +70,7 @@ linkList.prototype = {
 
 
 /**
- * 链反转
+ * 链反转（迭代方法）
  */
 function reverseList(node){
     if(node){
@@ -85,3 +85,128 @@ function reverseList(node){
         return arr[0]
     }
 }
+
+/**
+ * 用递归方式做链反转 （和迭代解法相比，虽然时间复杂度都是 O(N)，但是迭代解法的空间复杂度是 O(1)，而递归解法需要堆栈，空间复杂度是 O(N)）
+ * https://labuladong.gitbook.io/algo/shu-ju-jie-gou-xi-lie/2.2-shou-ba-shou-shua-lian-biao-ti-mu-xun-lian-di-gui-si-wei/di-gui-fan-zhuan-lian-biao-de-yi-bu-fen#yi-di-gui-fan-zhuan-zheng-ge-lian-biao
+ */
+
+ /**
+  * 1.反转整条链
+  * @param {*} node 
+  */
+ function reverse(node){
+     //base
+     if(node.next === null) return node
+
+     const last = reverse(node.next)
+     node.next.next = node
+     node.next = null
+     return last
+ }
+
+ /**
+  * 2.反转前n个
+  */
+ let mapNode = null
+ function reverseN(node,n){
+     // base
+     if(n==1){
+        mapNode = node.next
+        return node
+     }
+
+     const last = reverse(node,n-1)
+     node.next.next = node
+     node.next = mapNode
+     return last
+ }
+
+ /**
+  * 3反转第 m 到 n 个
+  */
+ function reverseMtoN(node,m,n){
+     // base
+     if(m==1){
+         return reverseN(node,n)
+     }
+     // 前进到反转的起点触发 base case
+    node.next = reverseMtoN(node.next,m-1,n-1)
+    return node
+ }
+
+ /**
+  * K个一组反转链 
+  * https://labuladong.gitbook.io/algo/shu-ju-jie-gou-xi-lie/2.2-shou-ba-shou-shua-lian-biao-ti-mu-xun-lian-di-gui-si-wei/k-ge-yi-zu-fan-zhuan-lian-biao
+  */
+
+  // 1.反转整条链路
+  function reverse(a){
+      let pre = null
+      let cur = a
+      let next = a
+      while(cur !=null){
+          next = cur.next
+          // 逐个节点反转
+          cur.next =pre
+          // 更新指针位置
+          pre = cur
+          cur = next
+      }
+      return pre
+  }
+
+  // 2.反转由节点a 到 节点b  反转区间 [a, b) 的元素，左闭右开
+  function reverseAtoB(a,b){
+      let pre = null
+      let cur = a
+      let next = a
+      while(cur !=b){
+          next =cur.next
+          cur.next = pre
+          pre = cur
+          cur = next
+      }
+      return pre
+  }
+
+  // 3. K个一组反转
+  function reverseKGroup(node,k){
+      // base 
+      if(node == null) return node
+      //反转区间 [a, b) 的元素，左闭右开
+      let a = node
+      let b = node
+      for(let i = 0;i<k;i++){
+          // 不足 k 个，不需要反转，base case
+          if(b===null) return node
+          b = b.next
+      }
+      // 先反转前k个
+      const newNode = reverseAtoB(a,b)
+      a.next = reverseKGroup(b,k)
+      return newNode
+  }
+
+
+  /**
+   * 判断回文链
+   * https://labuladong.gitbook.io/algo/shu-ju-jie-gou-xi-lie/2.2-shou-ba-shou-shua-lian-biao-ti-mu-xun-lian-di-gui-si-wei/pan-duan-hui-wen-lian-biao
+   */
+
+   function isPalindrome(head){
+    const array = []
+    while(head!==null){
+        array.push(head.val)
+        head = head.next
+    }
+    for(let i =0,j=array.length-1;i<j;i++,j--){
+        if(array[i]!==array[j]){
+            return false
+        }
+    }
+    return true
+
+   }
+
+
