82446992fe7909c041dbabd6cff0acf2657a9ec8
jiangdewei <jiangdewei02@maoyan.com>
2020-11-21 23:19:52 +0800

    feat: 数据流中位数 堆解法

--- 数据结构和算法/数据结构/堆/index.js
+++ 数据结构和算法/数据结构/堆/index.js
@@ -62,7 +62,124 @@ function MaxHeap(array){
      }
  }
 
+ //-------------------------------//
+ /**
+  * @description 通用封装实现
+  */
+ function Heap(type='min',value){
+     this.type = type;
+     this.value = []
+ }
+ Heap.prototype.create =function(values){
+     const length = this.value.length
+     const start = Math.floor(length/2)
+     for(let i = start;i>=0;i--){
+         this.ajust(i,length)
+     }
+ }
+ Heap.prototype.ajust = function(index,length){
+     const array = this.value;
+     for(let i = 2*index+1;i<length;i=2*i+1){
+         // i = 2*index+1 为左子叶
+         // i+1 为 右子叶
+        if(i+1<length){
+             if((this.type ==="max"&&array[i+1]>array[i])||
+                (this.type==="min"&&array[i+1]<array[i])){
+                i++
+             }
+        }
+        if((this.type ==='max'&&array[index]<array[i])||
+            (this.type=='min'&&array[index]>array[i])){
+            [array[index],array[i]] =[array[i],array[index]]
+            index = i
+        }else{
+            break
+        }
+     }
+ }
+ Heap.prototype.add =function(element){
+     const array = this.value
+     array.push(element)
+     if(array.length>1){
+         let index = array.length-1
+         let target = Math.floor((index-1)/2)
+         while(target>=0){
+            if((this.type==='min'&&array[index]<array[target])||
+                (this.type === 'max'&& array[index]>array[target])){
+                    [array[index],array[target]]= [array[target],array[index]]
+                    index = target
+                    target = Math.floor((index-1)/2)
+            }else{
+                break
+            }
+         }
+     }
+ }
 
+ Heap.prototype.pop = function(){
+     const array = this.value
+     let result = null
+     if(array.length>1){
+         result = array[0]
+         // 使用尾部元素填充头部
+         array[0] = array.pop()
+         this.ajust(0,array.length)
+     }else if(array.length === 1){
+         return array.pop()
+     }
+     return result
+ }
+
+
+ //demo
+ var heap = new Heap('max');
+ heap.add(6)
+ heap.add(10)
+ console.log(heap.value);
+ console.log(heap.pop());
+ console.log(heap.value);
+ 
+//-------------------------------//
+
+//----------数据流中位数--------------//
+//https://leetcode-cn.com/problems/find-median-from-data-stream/
+/**
+ * 解法大顶堆和小顶堆，奇数时，小顶堆比大顶堆数量大1
+ */
+var MedianFinder = function() {
+    this.count = 0
+    this.maxHeap = new Heap('max')
+    this.minHeap = new Heap('min')
+};
+
+/** 
+ * @param {number} num
+ * @return {void}
+ */
+MedianFinder.prototype.addNum = function(num) {
+    this.count++
+    if(this.count %2 === 1){
+        this.maxHeap.add(num)
+        this.minHeap.add(this.maxHeap.pop())
+    }else{
+        this.minHeap.add(num)
+        this.maxHeap.add(this.minHeap.pop())
+    }
+};
+
+/**
+ * @return {number}
+ */
+MedianFinder.prototype.findMedian = function() {
+    if(this.count %2 ===1){
+        return this.minHeap.value[0]
+    }else{
+        const result = (this.minHeap.value[0]+this.maxHeap.value[0])/2
+        return result
+    }
+};
+
+//----------------------------------//
 /**
  * TOPK
  */
